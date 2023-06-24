293da6ce873e7852f907a96f4020c6a99e721e5a
Archontis Kostis <arxontisk02@gmail.com>
2023-06-24 14:13:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 
 from fastapi import FastAPI
+import uvicorn
+from app import configure_cors
 from app.routers import routers
 
 app = FastAPI()
@@ -9,9 +11,8 @@ app = FastAPI()
 app.include_router(routers.router)
 
 logging.basicConfig(level=logging.DEBUG)
-
+configure_cors(app)
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
