a54dc16b783c33adb2281a5aed5c7a0e236f8cbf
Archontis Kostis <arxontisk02@gmail.com>
2023-06-24 14:10:50 +0300

    Update __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+from starlette.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+]
+
+logger = logging.getLogger(__name__)
+
+
+def configure_cors(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
