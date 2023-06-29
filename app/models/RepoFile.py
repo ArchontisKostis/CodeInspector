0510c7dd8fb2e7509445adca5995f076337334b2
ArchontisKostis <arxontisk02@gmail.com>
2023-06-30 02:08:47 +0300

    Update RepoFile.py

--- app/models/RepoFile.py
+++ app/models/RepoFile.py
@@ -6,7 +6,6 @@ class RepoFile:
         self.name = name
         self.metrics = {'CC': -1, 'NLOC': -1, 'CHURN': 0}
         self.priority = PriorityType.NOT_SET
-        self.code = ''
 
     def set_metric(self, metric_key: str, metric_value: int):
         key = str.upper(metric_key)
@@ -22,22 +21,15 @@ class RepoFile:
     def get_all_metrics(self) -> dict:
         return self.metrics
 
-    def set_priority(self, type):
-        self.priority = type
+    def set_priority(self, priority: PriorityType):
+        self.priority = priority
 
     def get_priority(self) -> PriorityType:
         return self.priority
-
-    def file_changed(self):
-        self.set_metric('CHURN', self.get_metric('CHURN') + 1)
-
-    def set_code(self, code: str):
-        self.code = code
 
     def to_dict(self):
         return {
             'name': self.name,
             'metrics': self.metrics,
             'priority': self.priority.name,
-            'code': self.code
         }
