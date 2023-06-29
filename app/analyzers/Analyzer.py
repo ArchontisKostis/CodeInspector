d7729df147606b6cda982b2fc99cad5a2a5f8298
ArchontisKostis <arxontisk02@gmail.com>
2023-06-30 02:08:16 +0300

    Update Analyzer.py

--- app/analyzers/Analyzer.py
+++ app/analyzers/Analyzer.py
@@ -9,15 +9,27 @@ class Analyser:
         self.analysis = Analysis(self.project)
         self.average_metric_finder = None
 
-    def find_max_complexity_file(self):
-        max_complexity = 0
-        max_complexity_file = None
+
+    def find_max_metric_file(self, metric_key: str):
+        max_metric_file = None
+
         for file in self.project.files:
-            if file.get_metric('CC') > max_complexity:
-                max_complexity = file.get_metric('CC')
-                max_complexity_file = file
+            if max_metric_file is None:
+                max_metric_file = file
+            elif file.get_metric(metric_key) > max_metric_file.get_metric(metric_key):
+                max_metric_file = file
+
+        return max_metric_file
+
+    def find_max_complexity_file(self):
+        max_complexity_file = self.find_max_metric_file('cc')
 
         self.analysis.set_max_complexity_file(max_complexity_file)
+
+    def find_max_churn_file(self):
+        max_churn_file = self.find_max_metric_file('churn')
+
+        self.analysis.set_max_churn_file(max_churn_file)
 
     def calculate_average_metrics(self):
         self.average_metric_finder = AverageMetricFinder(self.project)
@@ -30,6 +42,13 @@ class Analyser:
         self.analysis.set_avg_nloc(avg_nloc)
         self.analysis.set_avg_churn(avg_churn)
 
+    def calculate_total_lines_of_code(self):
+        total_loc = 0
+
+        for file in self.project.files:
+            total_loc += file.get_metric('NLOC')
+
+        self.analysis.total_nloc = total_loc
 
     def get_analysis(self):
         return self.analysis
