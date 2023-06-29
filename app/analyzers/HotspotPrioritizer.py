ca098bd2f2ff4ef801feec5ed216edaaaade9622
ArchontisKostis <arxontisk02@gmail.com>
2023-06-30 02:08:27 +0300

    Update HotspotPrioritizer.py

--- app/analyzers/HotspotPrioritizer.py
+++ app/analyzers/HotspotPrioritizer.py
@@ -7,34 +7,52 @@ class HotspotPriorityCalculator:
         self.files = []
         self.analysis = analysis
         self.outlier_eliminator = OutlierEliminator(analysis)
-        self.CC_THRESHOLD = 10
-        self.CHURN_THRESHOLD = 100
-        self.CC_PRIORITY_THRESHOLD = self.CC_THRESHOLD / 2
-        self.CHURN_PRIORITY_THRESHOLD = self.CHURN_THRESHOLD / 2
+        self.CC_THRESHOLD = 0
 
     def calculate_hotspot_priority(self):
         repo_files = self.analysis.project.files
-        
+
         for file in self.analysis.project.files:
-            cc = self.normalize_value(file.get_metric('CC'), 0, self.CC_THRESHOLD)
-            churn = self.normalize_value(file.get_metric('CHURN'), 0, self.CHURN_THRESHOLD)
+            cc = file.get_metric('CC')
+            churn = file.get_metric('CHURN')
+
             priority = self.calculate_priority(cc, churn)
+
             file.set_priority(priority)
-            file.set_metric('CC', cc)
-            file.set_metric('CHURN', churn)
 
     def calculate_priority(self, cc, churn):
-        if (cc >= self.CC_PRIORITY_THRESHOLD):
-            if (churn >= self.CHURN_PRIORITY_THRESHOLD):
-                return PriorityType.HIGH
-            else:
-                return PriorityType.MEDIUM
+        # We nedd to find the max CC and CHURN values
+        max_cc = self.analysis.max_complexity_file.get_metric('CC')
+        max_churn = self.analysis.max_churn_file.get_metric('CHURN')
 
+        # We need to find the middle point between the cc and the middle point between the churn
+        cc_middle_point = max_cc / 2
+        churn_middle_point = max_churn / 2
+
+        # if the cc is higher that the middle point and the churn is higher than the middle point
+        # the priority is high
+        if cc > cc_middle_point and churn > churn_middle_point:
+            return PriorityType.HIGH
+
+        # if the cc is higher that the middle point and the churn is lower than the middle point
+        # the priority is normal
+        elif cc > cc_middle_point and churn < churn_middle_point:
+            return PriorityType.NORMAL
+
+        # if the cc is lower that the middle point and the churn is higher than the middle point
+        # the priority is medium
+        elif cc < cc_middle_point and churn > churn_middle_point:
+            return PriorityType.MEDIUM
+
+        # if the cc is lower that the middle point and the churn is lower than the middle point
+        # the priority is low
+        elif cc < cc_middle_point and churn < churn_middle_point:
+            return PriorityType.LOW
+
+        # Else return not set
         else:
-            if (churn >= self.CHURN_PRIORITY_THRESHOLD):
-                return PriorityType.NORMAL
-            else:
-                return PriorityType.LOW
+            return PriorityType.NOT_SET
+
 
     def normalize_value(self, value, min_value, max_value):
         return (value - min_value) / (max_value - min_value)
