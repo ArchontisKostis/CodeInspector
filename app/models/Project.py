e1f0b70805dbad3575f8d1efed15d081720be1ab
ArchontisKostis <arxontisk02@gmail.com>
2023-06-30 02:08:33 +0300

    Update Project.py

--- app/models/Project.py
+++ app/models/Project.py
@@ -3,16 +3,18 @@ from app.models import RepoFile
 
 class Project:
     def __init__(self, repository_url: str):
+        self.project_name = 'Undefined Project Name'
         self.repository_url = repository_url
         self.files = []
 
     def add_file(self, file: RepoFile):
         if self.file_is_not_already_added(file):
+            print("Adding file: " + file.name)
+            file.set_metric('CHURN', 0)
             self.files.append(file)
-            file.file_changed()
         else:
-            repo_file = self.get_file(file.name)
-            repo_file.file_changed()
+            already_added_file = self.get_file(file.name)
+            already_added_file.set_metric('CHURN', already_added_file.get_metric('CHURN') + 1)
 
     def file_is_not_already_added(self, file):
         for curr_file in self.files:
@@ -32,6 +34,7 @@ class Project:
     # To dict method to return a dictionary representation of the project for the response
     def to_dict(self):
         return {
+            'project_name': self.project_name,
             'repository_url': self.repository_url,
             'files': [file.to_dict() for file in self.files]
         }
