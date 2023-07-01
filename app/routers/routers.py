61f18ff4986b08bea2357ef4896c424d5d5c8c30
ArchontisKostis <arxontisk02@gmail.com>
2023-07-02 00:36:35 +0300

    Update routers.py

--- app/routers/routers.py
+++ app/routers/routers.py
@@ -1,3 +1,5 @@
+import traceback
+
 from fastapi import APIRouter, HTTPException
 from pydriller import Repository
 
@@ -5,11 +7,9 @@ from app import logger
 from app.analyzers.Analyzer import Analyser
 from app.analyzers.CommitProcessor import CommitProcessor
 from app.analyzers.HotspotPrioritizer import HotspotPriorityCalculator
-import traceback
 from app.models.Project import Project
-from app.models.RepoFile import RepoFile
 from app.models.project_commit.ProjectCommitBuilder import ProjectCommitBuilder
-from app.routers import is_github_url, validate_repo_url
+from app.routers import validate_repo_url
 
 router = APIRouter()
 filetypes = ['.java']
@@ -28,6 +28,47 @@ async def perform_analysis(repo_url: str, from_date: str = None, to_date: str = 
 
         commit_processor = CommitProcessor(project)
         project_commits = []
+        project_name = "Undefined Project Name"
+
+        for commit in reps.traverse_commits():
+            if project_name == "Undefined Project Name":
+                project_name = commit.project_name
+
+            commit_processor.process_commit(commit)
+
+        analyzer = Analyser(project)
+
+        analyzer.calculate_total_lines_of_code()
+        analyzer.find_max_churn_file()
+        analyzer.find_max_complexity_file()
+
+        analyzer.calculate_average_metrics()
+
+        analysis = analyzer.get_analysis()
+        analysis.project_commits = project_commits
+        analysis.project.project_name = project_name
+
+        hotspot_calculator = HotspotPriorityCalculator(analysis)
+        hotspot_calculator.calculate_hotspot_priority()
+
+        return {"analysis": analysis.to_dict()}
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.get("/analysis/commits")
+def analyze_commits(repo_url: str, from_date: str = None, to_date: str = None):
+    try:
+
+        validate_repo_url(repo_url)
+
+        logger.info(f"Performing analysis on {repo_url}")
+
+        reps = Repository(repo_url, since=from_date, to=to_date)
+
+        project_commits = []
+        project_name = "Unknown"
         for commit in reps.traverse_commits():
             project_commit_builder = ProjectCommitBuilder()
 
@@ -50,22 +91,11 @@ async def perform_analysis(repo_url: str, from_date: str = None, to_date: str = 
 
             project_commits.append(project_commit)
 
-            commit_processor.process_commit(commit)
-
-        analyzer = Analyser(project)
-        analyzer.find_max_complexity_file()
-        analyzer.find_max_churn_file()
-        analyzer.calculate_total_lines_of_code()
-
-        analyzer.calculate_average_metrics()
-
-        analysis = analyzer.get_analysis()
-        analysis.project_commits = project_commits
-
-        hotspot_calculator = HotspotPriorityCalculator(analysis)
-        hotspot_calculator.calculate_hotspot_priority()
-
-        return {"analysis": analysis.to_dict()}
+        return {
+            "project_name": project_name,
+            "repo_url": repo_url,
+            "project_commits": [project_commit.to_dict() for project_commit in project_commits],
+        }
     except Exception as e:
         traceback.print_exc()
         raise HTTPException(status_code=400, detail=str(e))
