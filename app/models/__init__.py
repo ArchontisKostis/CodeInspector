64782addf6d9b415ea638f91badabfb9ed446f2e
ArchontisKostis <arxontisk02@gmail.com>
2023-07-03 21:54:54 +0300

    Remove unused function

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,1 @@
-def is_not_already_added(file, list):
-    try:
-        for item in list:
-            if file.filename == item.filename:
-                return False
-        return True
-    except Exception as e:
-        print("Error in is_not_already_added")
-        print(e)
+
