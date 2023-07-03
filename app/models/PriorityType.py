249e530a529af2afbb9698ec9763789ae2b36400
ArchontisKostis <arxontisk02@gmail.com>
2023-07-03 21:22:21 +0300

    Add a new priority type called UNKNOWN
    This is the value we will use when we don't know the priority of the file.
    NOT_SET will be th evalue we will use when the file is an outlier and also be the initial value

--- app/models/PriorityType.py
+++ app/models/PriorityType.py
@@ -6,4 +6,10 @@ class PriorityType(Enum):
     MEDIUM = "MEDIUM"
     NORMAL = "NORMAL"
     LOW = "LOW"
+
+    # This is the default value for the priority. Typically, this means that the file is an outlier
     NOT_SET = "NOT_SET"
+
+    # This is the value we will use when we don't know the priority of the file
+    # and does not match any of the cases in HotspotPriorityCalculator.py
+    UNKNOWN = "UNKNOWN"
