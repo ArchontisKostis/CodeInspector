cdf7d8b1944ac033fc1d0784bc8caaa41aaa7990
ArchontisKostis <arxontisk02@gmail.com>
2023-07-03 21:20:48 +0300

    Refactor Outlier Eliminator

--- app/analyzers/OutlierEliminator.py
+++ app/analyzers/OutlierEliminator.py
@@ -5,21 +5,18 @@ from app.models.Analysis import Analysis
 # Inliers are files that have a CC or Churn that is higher than the average CC or Churn of the project.
 # We will eliminate the outliers so that they do not skew the results of the analysis.
 class OutlierEliminator:
-    def __init__(self, analysis: Analysis):
-        self.analysis = analysis
-        self.project = analysis.project
+    def __init__(self, items_list: list, avg_cc: float, avg_churn: float):
+        self.items_list = items_list
+        self.avg_cc = avg_cc
+        self.avg_churn = avg_churn
 
-    def eliminate_outliers_based_on_cc_and_churn(self):
+    def eliminate_outliers(self):
         # To eliminate outliers based on CC and Churn, we will first calculate the average CC and Churn of all files in the project.
         # Then we will add all files that have a CC or Churn higher than 3 times the average CC or Churn to a list.
 
-        files = self.project.files
-        avg_cc = self.analysis.avg_cc
-        avg_churn = self.analysis.avg_churn
-
         no_outliers = []
-        for file in files:
-            if file.get_metric('CC') > avg_cc and file.get_metric('Churn') > avg_churn:
+        for file in self.items_list:
+            if file.get_metric('CC') > self.avg_cc or file.get_metric('Churn') > self.avg_churn:
                 no_outliers.append(file)
 
         return no_outliers
