cdab22a0e9cb5912fa13442ea869f98583530e76
ArchontisKostis <arxontisk02@gmail.com>
2023-07-04 18:11:39 +0300

    Refactor AverageMetricFinder.py

--- app/analyzers/AverageMetricFinder.py
+++ app/analyzers/AverageMetricFinder.py
@@ -5,20 +5,25 @@ class AverageMetricFinder:
     def __init__(self, project: Project):
         self.project_files = project.files
 
-    def calculate_avg_cc(self):
-        total_cc = 0
+    def calculate_average_metric(self, metric_name):
+        total_metric = 0
+        count = 0
         for file in self.project_files:
-            total_cc = total_cc + file.get_metric('CC')
-        return total_cc / len(self.project_files)
+            metric_value = file.get_metric(metric_name)
+            if metric_value is not None:
+                total_metric += metric_value
+                count += 1
+
+        if count > 0:
+            return total_metric / count
+        else:
+            return 0.0
+
+    def calculate_avg_cc(self):
+        return self.calculate_average_metric('CC')
 
     def calculate_avg_nloc(self):
-        total_nloc = 0
-        for file in self.project_files:
-            total_nloc = total_nloc + file.get_metric('NLOC')
-        return total_nloc / len(self.project_files)
+        return self.calculate_average_metric('NLOC')
 
     def calculate_avg_churn(self):
-        total_churn = 0
-        for file in self.project_files:
-            total_churn = total_churn + file.get_metric('CHURN')
-        return total_churn / len(self.project_files)
+        return self.calculate_average_metric('CHURN')
