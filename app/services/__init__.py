55f814c1ca931d352a2c27445d44034b269cf653
ArchontisKostis <arxontisk02@gmail.com>
2023-07-05 20:48:47 +0300

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -8,5 +8,11 @@ def calculate_past_year_date_range(from_date, to_date):
     from_date = one_year_ago
     to_date = today
 
-
     return from_date, to_date
+
+
+def try_to_parse_date(date):
+    try:
+        return datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Invalid date format. Please use the format YYYY-MM-DD.")
