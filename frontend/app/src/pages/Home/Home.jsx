007a0184cc9b59beb8671591c50273d0f51c9c91
ArchontisKostis <arxontisk02@gmail.com>
2023-07-08 19:17:16 +0300

    Create Home Page boilerplate

---
+++ frontend/app/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+// Home Page Component
+// Description: This is the home page component. It is the first page that the user sees when they visit the website.
+
+import React from 'react';
+
+const Home = (props) => {
+
+    return (
+        <>
+            <div className="home-container">
+                <h1>Home Page</h1>
+            </div>
+        </>
+    );
+}
+
+export default Home;
