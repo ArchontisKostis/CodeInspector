330486a613c1d0f82ab67c5514a4c40607f97ca2
ArchontisKostis <arxontisk02@gmail.com>
2023-07-08 22:11:19 +0300

    Create Hotspot Analysis

---
+++ frontend/app/src/components/HotspotAnalysis/HotspotAnalysis.jsx
@@ -0,0 +1,18 @@
+// HotspotAnalysis Component
+// Description: Component for the Hotspot analysis page
+
+import React from 'react';
+
+import './HotspotAnalysis.css';
+
+const HotspotAnalysis = () => {
+    return (
+        <>
+            <div className="hotspot-analysis">
+                <h1>Hotspot Analysis</h1>
+            </div>
+        </>
+    );
+}
+
+export default HotspotAnalysis;
