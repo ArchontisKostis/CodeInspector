5a3040a6307c5d955b6975a569e93d46128d98a0
ArchontisKostis <arxontisk02@gmail.com>
2023-07-09 22:36:54 +0300

    Create Techniques Section

---
+++ frontend/app/src/ui/home/TechniquesSection/TechniquesSection.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import './TechniquesSection.css';
+import TechniqueBox from "../TechniqueBox/TechniqueBox.jsx";
+
+const TechniquesSection = () => {
+    return (
+        <div className="techniques-section">
+            <h2>Sophisticated Analysis Techniques</h2>
+            <article>
+                At CodeInspector, we employ sophisticated analysis techniques to deliver
+                comprehensive insights into your codebase. Our app leverages the Delta Maintainability
+                Model (DMM) and the Eisenhower Matrix to help you make informed decisions and drive impactful
+                improvements.
+            </article>
+
+            <TechniqueBox
+                title="Delta Maintainability Model"
+                description={
+                    "Our tool utilizes the Delta Maintainability Model (DMM) to evaluate " +
+                    "the maintainability implications of code changes. By categorizing commits " +
+                    "based on DMM complexity, we provide insights into the quality of each change, " +
+                    "empowering you to make informed decisions during code reviews and further enhancements."
+                }
+            />
+
+            <TechniqueBox
+                title="Eisenhower Matrix"
+                description={
+                    "Our hotspot prioritization technique draws inspiration from the renowned " +
+                    "Eisenhower Matrix. By considering complexity and code churn, we identify critical " +
+                    "areas that require immediate attention, helping you streamline your efforts and focus " +
+                    "on areas that will yield the greatest impact."
+                }
+            />
+
+            <div className="triangle-seperator"></div>
+        </div>
+    );
+}
+
+export default TechniquesSection;
