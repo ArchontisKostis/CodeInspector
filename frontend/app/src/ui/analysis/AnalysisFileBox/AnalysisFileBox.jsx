d08318501ea79c342010e9610a700bdbfcc99957
ArchontisKostis <arxontisk02@gmail.com>
2023-07-09 12:23:48 +0300

    Update analysis file box

--- frontend/app/src/ui/analysis/AnalysisFileBox/AnalysisFileBox.jsx
+++ frontend/app/src/ui/analysis/AnalysisFileBox/AnalysisFileBox.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-
+import "./AnalysisFileBox.css";
 
 const AnalysisFileBox = (props) => {
     const { fileName, cc, churn, nloc } = props;
 
     return (
         <>
-            <div className="analysis-info-box-container">
-                <div className="analysis-info-box">
+            <div className="analysis-file-box-container">
+                <div className="analysis-file-box">
                     <h3>{fileName}</h3>
                     <p><b>CC:</b> {cc}</p>
                     <p><b>CHURN:</b> {churn}</p>
