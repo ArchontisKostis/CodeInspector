49a249b136ebd995dc64cf439ea48a7579344df7
ArchontisKostis <arxontisk02@gmail.com>
2023-07-09 17:38:41 +0300

    Update AnalysisInfoBox.jsx

--- frontend/app/src/ui/analysis/AnalysisInfoBox/AnalysisInfoBox.jsx
+++ frontend/app/src/ui/analysis/AnalysisInfoBox/AnalysisInfoBox.jsx
@@ -5,14 +5,23 @@ import './AnalysisInfoBox.css';
 const AnalysisInfoBox = (props) => {
     const { projectName, totalCommits, fromDate, toDate, githubUrl } = props;
 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
+
     return (
         <>
             <div className="analysis-info-box-container">
                 <div className="analysis-info-box">
                     <h3>{projectName}</h3>
                     <p>Total Commits: {totalCommits}</p>
-                    <p>From: {fromDate}</p>
-                    <p>To: {toDate}</p>
+                    <p>From: {formatDate(fromDate)}</p>
+                    <p>To: {formatDate(toDate)}</p>
                     <a href={githubUrl} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                 </div>
             </div>
