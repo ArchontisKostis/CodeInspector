11a4aeaeaaaa5c5febf9d458cfd8dfebe99e3540
ArchontisKostis <arxontisk02@gmail.com>
2023-07-10 17:14:55 +0300

    Update AnalysisPage.jsx

--- frontend/app/src/pages/AnalysisPage/AnalysisPage.jsx
+++ frontend/app/src/pages/AnalysisPage/AnalysisPage.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './AnalysisPage.css';
 import AnalysisForm from "../../components/AnalysisForm/AnalysisForm.jsx";
 import HotspotAnalysis from "../../components/HotspotAnalysis/HotspotAnalysis.jsx";
 import useFetch from "../../hooks/useFetch.js";
 import Wave from "../../ui/Wave/Wave.jsx";
+import {useLocation} from "react-router-dom";
+import CommitAnalysis from "../../components/CommitAnalysis/CommitAnalysis.jsx";
 
 const defaultOptions = {
     method: 'GET',
 };
 
 const AnalysisPage = (props) => {
-    const { isLoading, error, response } = useFetch(
-        'http://localhost:8000/api/analysis/prioritize_hotspots?repo_url=https://github.com/GeorgeApos/pyassess',
-        defaultOptions
-    );
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search);
+    const repoUrl = queryParams.get('repoUrl');
+    const fromDate = queryParams.get('fromDate');
+    const toDate = queryParams.get('toDate');
+    const analysisType = queryParams.get('analysisType');
 
+    const [apiUrl, setApiUrl] = useState('');
 
-    const { analysis } = response || {};
+    useEffect(() => {
+        let constructedApiUrl = '';
+
+        if (analysisType === 'hotspot-prioritization') {
+            constructedApiUrl = `http://localhost:8000/api/analysis/prioritize_hotspots?repo_url=${encodeURIComponent(repoUrl)}`;
+        } else if (analysisType === 'commit-analysis') {
+            constructedApiUrl = `http://localhost:8000/api/analysis/commits?repo_url=${encodeURIComponent(repoUrl)}`;
+        }
+
+        if (fromDate && toDate) {
+            constructedApiUrl = `${constructedApiUrl}&from_date=${encodeURIComponent(fromDate)}&to_date=${encodeURIComponent(toDate)}`;
+        } else if (fromDate) {
+            constructedApiUrl = `${constructedApiUrl}&from_date=${encodeURIComponent(fromDate)}`;
+        } else if (toDate) {
+            constructedApiUrl = `${constructedApiUrl}&to_date=${encodeURIComponent(toDate)}`;
+        }
+
+        setApiUrl(constructedApiUrl);
+    }, [repoUrl, fromDate, toDate, analysisType]);
+
+    const { isLoading, error, response } = useFetch(apiUrl);
+
+    const analysis = response?.analysis || {};
+
     const {
         repo_url,
         from_date,
@@ -33,10 +61,6 @@ const AnalysisPage = (props) => {
         prioritized_files,
         outliers,
     } = analysis || {};
-
-    // Calculate progress percentage
-    const progress = response ? 100 : isLoading ? 0 : -1;
-
 
     return (
         <>
@@ -65,12 +89,11 @@ const AnalysisPage = (props) => {
                 ) : (
                     response && (
                         <>
-                            <HotspotAnalysis data={analysis} />
-
-                            <button className="export-to-csv-btn">
-                                <i className="bi bi-filetype-csv"></i>
-                                <p>Export to CSV</p>
-                            </button>
+                            {analysisType === 'hotspot-prioritization' ? (
+                                <HotspotAnalysis data={analysis} />
+                            ) : (
+                                <CommitAnalysis />
+                            )}
                         </>
                     )
                 )}
