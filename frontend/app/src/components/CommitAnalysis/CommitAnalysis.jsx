47a37a744e4dde5905b283df4aadfe784aa76aa2
ArchontisKostis <arxontisk02@gmail.com>
2023-07-10 17:01:41 +0300

    Create Commit Analysis Page

---
+++ frontend/app/src/components/CommitAnalysis/CommitAnalysis.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './CommitAnalysis.css';
+
+const CommitAnalysis = (props) => {
+    return (
+        <>
+            <div className="commit-analysis-container">
+                <h1 className="commit-analysis-header">Commit Analysis</h1>
+            </div>
+        </>
+    );
+}
+
+export default CommitAnalysis;
