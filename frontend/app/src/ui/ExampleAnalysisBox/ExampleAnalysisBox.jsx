13b69cd4be0ed3150b1759d827092cc5991e9d22
ArchontisKostis <arxontisk02@gmail.com>
2023-07-10 21:55:31 +0300

    Update ExampleAnalysisBox

--- frontend/app/src/ui/ExampleAnalysisBox/ExampleAnalysisBox.jsx
+++ frontend/app/src/ui/ExampleAnalysisBox/ExampleAnalysisBox.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 import './ExampleAnalysisBox.css';
+import {useNavigate} from "react-router-dom";
 const ExampleAnalysisBox = (props) => {
-    const { project_name, repo_url, analysis_from_date, analysis_to_date } = props;
+    const { project_name, repo_url, analysis_from_date, analysis_to_date, analysis_type } = props;
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        console.log("Clicked");
+
+        let params = { repoUrl: repo_url, fromDate: analysis_from_date, toDate: analysis_to_date, analysisType: analysis_type };
+
+        const queryParams = new URLSearchParams(params);
+        const queryString = queryParams.toString();
+
+        navigate(`/analysis?${queryString}`);
+    }
 
     return (
-        <div className="example-analysis-box">
+        <div className="example-analysis-box" onClick={handleClick}>
             <div className="example-analysis-box-header">
                 <i className="bi bi-github"> {project_name}</i>
             </div>
@@ -15,6 +28,9 @@ const ExampleAnalysisBox = (props) => {
                 <a href={repo_url}>Click Here</a>
                 <h3>Analysis Date: </h3>
                 <p>{analysis_from_date} - {analysis_to_date}</p>
+
+                <h3>Analysis Type: </h3>
+                <p>{analysis_type}</p>
             </div>
         </div>
     );
