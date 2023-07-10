b92520e15855dc9f5d666a8e668664abf3ef17c5
ArchontisKostis <arxontisk02@gmail.com>
2023-07-10 18:40:10 +0300

    Add custom Loading bar

---
+++ frontend/app/src/ui/LoadingBar/LoadingBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./LoadingBar.css";
+
+const LoadingBar = () => {
+    return (
+        <div className="loader">
+            <div className="loader-bar"></div>
+        </div>
+    );
+}
+
+export default LoadingBar;
