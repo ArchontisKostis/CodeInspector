c7005d018a6349a13a2bdf3c7bb055f0350e8e13
ArchontisKostis <arxontisk02@gmail.com>
2023-07-19 21:48:35 +0300

    Create Error Page

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home.jsx";
 import AnalysisPage from "./pages/AnalysisPage/AnalysisPage.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import ToolPage from "./pages/ToolPage/ToolPage.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 function App() {
 
   return (
@@ -17,6 +18,8 @@ function App() {
                     <Route exact path='/' element={<Home />} />
                     <Route path='/analysis/*' element={<AnalysisPage />} />
                     <Route exact path='/tool' element={<ToolPage />} />
+                    {/* Error Page */}
+                    <Route path='*' element={<ErrorPage />} />
                 </Routes>
 
                 <Footer />
