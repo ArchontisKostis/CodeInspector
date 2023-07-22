0724bcc07622201192c7514b953d13f91f0cb092
ArchontisKostis <arxontisk02@gmail.com>
2023-07-22 18:14:18 +0300

    Update ErrorPage

--- frontend/app/src/pages/ErrorPage/ErrorPage.jsx
+++ frontend/app/src/pages/ErrorPage/ErrorPage.jsx
@@ -8,15 +8,19 @@ import {Link} from "react-router-dom";
 const ErrorPage = () => {
     return (
         <>
-        <div className="error-page">
-            <img src={errorImg} alt="404 Error. Page Not Found." className="error-svg"/>
-            <p className="error-404-msg">Sorry, the page you're looking for doesn't exist.</p>
-            <Link to="/" className="error-btn">Go Home</Link>
+            <div className="eror-page-container">
+                <div className="error-page-content">
+                    <img src={errorImg} alt="404 Error. Page Not Found." className="error-svg"/>
+                    <p className="error-404-msg">Sorry, the page you're looking for doesn't exist.</p>
+                    <Link to="/" className="error-btn">Go Home</Link>
 
-        </div>
-        <Wave
-            waveStyle="light" />
+                </div>
+                <Wave
+                    waveStyle="light" />
+
+            </div>
         </>
+
     );
 }
 
