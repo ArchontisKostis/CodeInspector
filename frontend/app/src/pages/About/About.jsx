24ff839c7965190607c61e7dbb3eeaf4df8ae33b
ArchontisKostis <arxontisk02@gmail.com>
2023-07-23 17:56:33 +0300

    Make SimpleInfoBox Component

--- frontend/app/src/pages/About/About.jsx
+++ frontend/app/src/pages/About/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./About.css";
 import Wave from "../../ui/Wave/Wave.jsx";
+import SimpleInfoBox from "../../ui/boxes/SimpleInfoBox/SimpleInfoBox.jsx";
 
 const About = () => {
     return (
@@ -32,24 +33,15 @@ const About = () => {
                     that exhibit high complexity and frequent changes.
                 </p>
 
-                <div className="simple-info-box">
-                    <h3>Hotspot Finding</h3>
+                <SimpleInfoBox
+                    title="Hotspot Finding"
+                    content="To ensure accurate prioritization, we perform hotspot finding to identify areas of the codebase that exhibit significant complexity and frequent changes. This step allows us to pinpoint critical sections that require closer examination."
+                />
 
-                    <p>
-                        To ensure accurate prioritization, we perform hotspot finding to identify
-                        areas of the codebase that exhibit significant complexity and frequent changes.
-                        This step allows us to pinpoint critical sections that require closer examination.
-                    </p>
-                </div>
-
-                <div className="simple-info-box">
-                    <h3>Calculation of Hotspot Priority</h3>
-
-                    <p>
-                        To calculate the priority of hotspots, we adopt an approach similar to the Eisenhower Matrix. In our context, we adapt the matrix to prioritize hotspots based on their complexity and code churn. <br/>
-                        By considering both complexity and churn metrics, we assess the significance and impact of each hotspot on software quality and maintainability. The priority calculation takes into account the relative position of a hotspot's metrics compared to the maximum CC and churn values observed in the project.
-                    </p>
-                </div>
+                <SimpleInfoBox
+                    title="Calculation of Hotspot Priority"
+                    content="To calculate the priority of hotspots, we adopt an approach similar to the Eisenhower Matrix. In our context, we adapt the matrix to prioritize hotspots based on their complexity and code churn. By considering both complexity and churn metrics, we assess the significance and impact of each hotspot on software quality and maintainability. The priority calculation takes into account the relative position of a hotspot's metrics compared to the maximum CC and churn values observed in the project."
+                />
 
                 <Wave waveStyle="dark" />
             </div>
@@ -63,34 +55,20 @@ const About = () => {
                     section, we gain insights into the maintainability and risk associated with commits.
                 </p>
 
-                <div className="simple-info-box">
-                    <h3>Commit Retrieval</h3>
+                <SimpleInfoBox
+                    title="Commit Retrieval"
+                    content="To ensure accurate prioritization, we perform hotspot finding to identify areas of the codebase that exhibit significant complexity and frequent changes. This step allows us to pinpoint critical sections that require closer examination."
+                />
 
-                    <p>
-                        To ensure accurate prioritization, we perform hotspot finding to identify areas of the
-                        codebase that exhibit significant complexity and frequent changes. This step allows us to
-                        pinpoint critical sections that require closer examination.
-                    </p>
-                </div>
+                <SimpleInfoBox
+                    title="DMM Metrics Calculation"
+                    content="To assess the maintainability implications of commits, we utilize the Open Source Delta Maintainability Model (OS-DMM) implemented by PyDriller. This model, described in a paper at TechDebt 2019 [DiBiase2019], provides insights into the maintainability of code changes. The DMM metrics take into account unit size, unit complexity, and unit interfacing."
+                />
 
-                <div className="simple-info-box">
-                    <h3>DMM Metrics Calculation</h3>
-
-                    <p>
-                        To assess the maintainability implications of commits, we utilize the Open Source Delta Maintainability Model (OS-DMM) implemented by PyDriller. This model, described in a paper at TechDebt 2019 [DiBiase2019], provides insights into the maintainability of code changes. <br/>
-                        The DMM metrics take into account unit size, unit complexity, and unit interfacing.
-                    </p>
-                </div>
-
-                <div className="simple-info-box">
-                    <h3>Change Categorization</h3>
-
-                    <p>
-                        Based on the computed DMM complexity value, we categorize each commit into different categories.
-                        <br/>
-                        To assess the risk category based on the computed DMM value, we have defined several categories, such as EXCELLENT, GOOD, FAIR, POOR, and UNKNOWN. These categories provide an indication of the maintainability implications of each commit.
-                    </p>
-                </div>
+                <SimpleInfoBox
+                    title="Change Categorization"
+                    content="Based on the computed DMM complexity value, we categorize each commit into different categories. To assess the risk category based on the computed DMM value, we have defined several categories, such as EXCELLENT, GOOD, FAIR, POOR, and UNKNOWN. These categories provide an indication of the maintainability implications of each commit."
+                />
 
                 <Wave waveStyle="light" />
             </div>
