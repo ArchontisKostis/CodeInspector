4d307d7d80e8c1f3a24e8c96d9a2e072685d2124
ArchontisKostis <arxontisk02@gmail.com>
2023-10-25 14:50:46 +0300

    Update main.py
    Connect to the database

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@ import logging
 
 from fastapi import FastAPI
 import uvicorn
+from sqlalchemy import create_engine
 
 from app import configure_cors
+from app.config.dbConfig import DATABASE_URL
 from app.routers import analysis_router as routers
 
 app = FastAPI()
@@ -19,5 +21,9 @@ HOST_IP = '127.0.0.1'
 PORT = 8000
 
 if __name__ == "__main__":
+
+    db_engine = create_engine(DATABASE_URL)
+    db_connection = db_engine.connect()
+
     uvicorn.run(app, host=HOST_IP, port=PORT)
 
