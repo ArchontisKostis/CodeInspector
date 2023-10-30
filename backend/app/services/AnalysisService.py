fbb8a54a2b33e3d664fac2943d08c62a9f24f1d2
ArchontisKostis <arxontisk02@gmail.com>
2023-10-30 15:17:04 +0200

    Update AnalysisService.py
    Make the class have a session argument and use it to save objects to the database. /
    
    Also make the commit analysis save the data to sql tables

--- backend/app/services/AnalysisService.py
+++ backend/app/services/AnalysisService.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 from pydriller import Repository
+from sqlalchemy.orm import Session
 
 from app.analyzers.Analyzer import Analyser
 from app.analyzers.CommitProcessor import CommitProcessor
 from app.analyzers.DmmAnalyzer import DmmAnalyzer
+from app.db.entities.CommitAnalysisTable import CommitAnalysisTable
+from app.db.entities.ProjectCommitTable import ProjectCommitTable
+from app.db.entities.ProjectTable import ProjectTable
 from app.models.Project import Project
 from app.models.analysis.CommitAnalysis import CommitAnalysis
 from app.models.project_commit.ProjectCommitBuilder import ProjectCommitBuilder
@@ -14,8 +18,9 @@ from app.exceptions.NoCommitsException import NoCommitsException
 
 
 class AnalysisService:
-    def __init__(self):
+    def __init__(self, session):
         self.filetypes = ['.java']
+        self.session = session
 
     def analyze_hotspots(self, repo_url: str, from_date: str, to_date: str):
         from_date, to_date = validate_date(from_date, to_date)
@@ -77,5 +82,48 @@ class AnalysisService:
             project_commit.change_category = dmm_analyzer.find_commit_rating(project_commit)
 
             commit_analysis.add_commit(project_commit)
+
+        # Now save everything to the database
+        db_project = ProjectTable()
+        db_project.project_name = commit_analysis.project_name
+        db_project.repository_url = repo_url
+
+        self.session.add(db_project)
+        self.session.commit()
+        self.session.refresh(db_project)
+
+        db_analysis = CommitAnalysisTable()
+        db_analysis.project_id = db_project.id
+        db_analysis.from_date = from_date
+        db_analysis.to_date = to_date
+        db_analysis.total_commits = len(commit_analysis.commits)
+
+        self.session.add(db_analysis)
+        self.session.commit()
+        self.session.refresh(db_analysis)
+
+        for commit in commit_analysis.commits:
+            db_commit = ProjectCommitTable()
+            db_commit.analysis_id = db_analysis.id
+            db_commit.hash = commit.hash
+            db_commit.committer = commit.committer
+            db_commit.author = commit.author
+            db_commit.committer_date = commit.committer_date
+            db_commit.author_date = commit.author_date
+            db_commit.number_of_added_lines = commit.number_of_added_lines
+            db_commit.number_of_deleted_lines = commit.number_of_deleted_lines
+            db_commit.number_of_files_changed = commit.number_of_files_changed
+            db_commit.author_email = commit.author_email
+            db_commit.committer_email = commit.committer_email
+            db_commit.dmm_unit_interfacing = commit.dmm_unit_interfacing
+            db_commit.dmm_unit_complexity = commit.dmm_unit_complexity
+            db_commit.dmm_unit_size = commit.dmm_unit_size
+            db_commit.dmm_score = commit.dmm_score
+            db_commit.change_category = str(commit.change_category)
+
+            self.session.add(db_commit)
+            self.session.commit()
+            self.session.refresh(db_commit)
+
 
         return commit_analysis
